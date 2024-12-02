45e92f9a6ff366205a4f23e0fb07106ff95df3e0
Jhelison Uchoa <jhelison@brickabode.com>
2024-12-02 00:57:45 -0300

    docs: Add docs to the project

--- scripts/rps.sh
+++ scripts/rps.sh
@@ -4,7 +4,7 @@
 # https://golang.testcontainers.org/
 
 # YOU MUST HAVE A CHAIN RUNNING TO EXECUTE THIS SCRIPT
-# make init && rpsd start --minimum-gas-prices 0rps
+# make init && rpsd start --minimum-gas-prices 0rps --api.enable true
 
 # Variables
 NAME1="alice"
@@ -22,10 +22,12 @@ ADDRESS1=$($BIN keys show $NAME1 -a --keyring-backend test)
 ADDRESS2=$($BIN keys show $NAME2 -a --keyring-backend test)
 
 # Create a new student for alice
-$BIN tx rps create_student alice $ADDRESS1 20 --from $NAME1 -o json -y | jq
+TX_HASH=$($BIN tx rps create_student alice $ADDRESS1 20 --from $NAME1 -o json -y | jq ".txhash")
+echo "Student for alice created with tx hash $TX_HASH"
 
 # Create a new student for bob
-$BIN tx rps create_student bod $ADDRESS2 45 --from $NAME2 -o json -y | jq
+TX_HASH=$($BIN tx rps create_student bod $ADDRESS2 45 --from $NAME2 -o json -y | jq ".txhash")
+echo "Student for bob created with tx hash $TX_HASH"
 
 sleep 5
 
@@ -46,7 +48,9 @@ $BIN query rps student $ADDRESS2 -o json | jq
 #####################
 
 echo "Deleting the bob student"
-$BIN tx rps delete_student $ADDRESS2 --from $NAME2 -o json -y | jq
+
+TX_HASH=$($BIN tx rps delete_student $ADDRESS2 --from $NAME2 -o json -y | jq ".txhash")
+echo "Student for bob deleted with tx hash $TX_HASH"
 
 sleep 5
 
