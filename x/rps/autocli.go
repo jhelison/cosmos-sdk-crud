2fa8ac39d227394ef441a1fecff42d410ba7de2c
Jhelison Uchoa <jhelison@brickabode.com>
2024-12-01 23:33:57 -0300

    feat(query): Add the create and delete student CLI commands

--- x/rps/autocli.go
+++ x/rps/autocli.go
@@ -14,10 +14,43 @@ import (
 func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 	// Get the bech32 address for the commands
 	bech32PrefixAccAddr := sdk.GetConfig().GetBech32AccountAddrPrefix()
+	// The app name
+	appName := version.AppName
 
 	// Return the CLI commands
 	return &autocliv1.ModuleOptions{
-		Tx: &autocliv1.ServiceCommandDescriptor{},
+		// All the txs
+		Tx: &autocliv1.ServiceCommandDescriptor{
+			Service: rpsv1.Msg_ServiceDesc.ServiceName,
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				// The createStudent message
+				{
+					RpcMethod: "CreateStudent",
+					Use:       "create_student [name] [id] [age]",
+					Short:     "Create a new student based on a name, ID and age",
+					Long: fmt.Sprintf(`Create a new student based on a name and age
+Example
+$ %s tx rps create_student kii 20`, bech32PrefixAccAddr),
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "name"},
+						{ProtoField: "id"},
+						{ProtoField: "age"},
+					},
+				},
+				// The deleteStudent message
+				{
+					RpcMethod: "DeleteStudent",
+					Use:       "delete_student [id]",
+					Short:     "Delete a student based on it's ID",
+					Long: fmt.Sprintf(`Delete a student based on it's ID
+Example
+$ %s tx rps delete_student kii 20`, bech32PrefixAccAddr),
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "id"},
+					},
+				},
+			},
+		},
 		// All the queries
 		Query: &autocliv1.ServiceCommandDescriptor{
 			Service: rpsv1.Query_ServiceDesc.ServiceName,
@@ -29,7 +62,7 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Short:     "Query a student by it's ID",
 					Long: fmt.Sprintf(`Query a student by it's ID.
 Example
-$ %s query rps student %s1gghjut3ccd8ay0zduzj64hwre2fxs9ld75ru9p`, version.AppName, bech32PrefixAccAddr),
+$ %s query rps student %s1gghjut3ccd8ay0zduzj64hwre2fxs9ld75ru9p`, appName, bech32PrefixAccAddr),
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
 						{ProtoField: "id"},
 					},
